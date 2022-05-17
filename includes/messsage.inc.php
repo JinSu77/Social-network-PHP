44439e4f62f68daa8f3fc44dee5a8c6e5fa9d5f4
Setsu <60669080+Setsudan@users.noreply.github.com>
2022-05-17 14:39:31 +0100

    Ajout de fonctionnalité msg a debug

---
+++ includes/messsage.inc.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_GET['id']) and !empty($_GET['id'])) {
+
+    $getid = $_GET['id'];
+    //!  changer la requetes en dessous pour récup les messages du channel et pas de l'utilisateur
+    //* Le reste je te laisse gérer mais fait en sorte qu'on récup une array ou un objet
+    $recupUser = $bdd->prepare('SELECT * FROM message WHERE id = ?');
+    $recupUser->execute(array($getid));
+    if ($recupUser->rowCount() > 0) {
+        if (isset($_POST['envoyer'])) {
+            $message = htmlspecialchars($_POST['message']);
+            $insererMessage = $bdd->prepare('INSERT INTO chanel(messages, id_destinataire, id_auteur)VALUES(?, ?, ?)');
+            $insererMessage->execute(array($message, $getid, $_SESSION['id']));
+        }
+    } else {
+        echo "aucun user trouver";
+    }
+} else {
+    echo "aucun id à était trouver";
+}
