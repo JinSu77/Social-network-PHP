b60b2e2bbd6aba80c1a94b4c6da3223504b8257d
gaoubak <80465380+gaoubak@users.noreply.github.com>
2022-05-17 15:00:39 +0200

    friend exist

--- includes/friends.inc.php
+++ includes/friends.inc.php
@@ -12,13 +12,28 @@ class Friends
     {
         $this->bdd = $bdd;
     }
-
+    public function FriendsExist($uid,$follower_id)
+    {
+     $request = $this->bdd->prepare("SELECT FROM follower where user_id = ? AND follower_id = ?");
+     $request->execute([$uid]);
+     $resultat = $request->fetchAll(PDO::FETCH_ASSOC);
+    }
     public function AddFriends($uid, $follower_id)
     {
+     $exit = $this->FriendsExist($uid,$follower_id);
+     if(!isset($exit)){
+         return null;
+     }
+     else {
+         $request = $this->bdd->prepare("INSERT INTO follower(user_id, follower_id) VALUES (?,?)" );
+        $request->execute([$uid,$follower_id]);
+        return true;
+    }
+     
+    }
 
-     $request = $this->bdd->prepare("INSERT INTO follower(user_id, follower_id) VALUES (?,?)" );
-     $request->execute([$uid,$follower_id]);
-     return true;
+    public function CreateChannel($uid){
+     $request = $this->bdd->prepare("INSERT INTO chanel(user_id, )");
 
     }
 }
