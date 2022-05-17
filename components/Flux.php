8567ec4e459b6c610f1d2a8124ed7cdc4561105d
Setsu <60669080+Setsudan@users.noreply.github.com>
2022-05-17 15:30:22 +0100

    correction head & others

--- components/Flux.php
+++ components/Flux.php
@@ -25,46 +25,47 @@
     </div>
 </span>
 <script>
-const allPost = document.getElementById("allPost")
-setInterval(
-    fetch("./includes/fetchPost.inc.php", {
-        method: "GET",
-    })
-    .then((resp) => resp.text()).then((json) => {
-        let data = JSON.parse(json)
-        console.log(data);
-        data.forEach(post => {
-            let postContainer = document.createElement("div")
-            postContainer.classList.add("main-post")
-            let postHeader = document.createElement("div")
-            postHeader.classList.add("header-post")
-            postContainer.appendChild(postHeader)
+    const allPost = document.getElementById("allPost")
+    setInterval(
+        fetch("./includes/fetchPost.inc.php", {
+            method: "GET",
+        })
+        .then((resp) => resp.text()).then((json) => {
+            let data = JSON.parse(json)
+            console.log(data);
+            data.forEach(post => {
+                let postContainer = document.createElement("div")
+                postContainer.classList.add("main-post")
 
-            let profilePost = document.createElement("div")
-            profilePost.classList.add("profil-post")
-            postHeader.appendChild(profilePost)
+                let postHeader = document.createElement("div")
+                postHeader.classList.add("header-post")
 
-            let userDOM = document.createElement("div")
-            userDOM.classList.add("name-date-post")
-            userDOM.innerText = post.username
-            profilePost.appendChild(userDOM)
+                let profilePost = document.createElement("div")
+                profilePost.classList.add("profil-post")
 
-            let contentPost = document.createElement("div")
-            contentPost.classList.add("content-post")
-            contentPost.innerText = post.post_text
-            let date = document.createElement("span")
-            date.classList.add("date")
-            date.innerText = post.post_date
+                let userDOM = document.createElement("div")
+                userDOM.classList.add("name-date-post")
+                userDOM.innerText = post.username
 
-            let PostImg = document.createElement("img")
-            PostImg.classList.add("postImg")
-            PostImg.src = post.post_img
-            contentPost.appendChild(PostImg)
+                let contentPost = document.createElement("div")
+                contentPost.classList.add("content-post")
+                contentPost.innerText = post.post_text
+                let date = document.createElement("span")
+                date.classList.add("date")
+                date.innerText = post.post_date
 
-            contentPost.appendChild(date)
-            postContainer.appendChild(contentPost)
-            allPost.appendChild(postContainer)
-        });
-    }), 1000
-)
+                let PostImg = document.createElement("img")
+                PostImg.classList.add("postImg")
+                PostImg.src = post.post_img
+
+                postHeader.appendChild(PostImg)
+                postContainer.appendChild(postHeader)
+                postHeader.appendChild(profilePost)
+                profilePost.appendChild(userDOM)
+                contentPost.appendChild(date)
+                postContainer.appendChild(contentPost)
+                allPost.appendChild(postContainer)
+            });
+        }), 1000
+    )
 </script>
