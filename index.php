238a2cdedbfbf156f94236567cfebb8ef870799c
Setsu <60669080+Setsudan@users.noreply.github.com>
2022-05-18 08:18:10 +0100

    remove unecessary images

--- index.php
+++ index.php
@@ -10,6 +10,55 @@ require_once "./components/header.php" ?>
     }
     ?>
 </div>
+<script>
+const allPost = document.getElementById("allPost");
+const fetchPost = () => {
+    fetch("./includes/fetchPost.inc.php", {
+            method: "GET",
+        })
+        .then((resp) => resp.text())
+        .then((json) => {
+            let data = JSON.parse(json);
+            console.log(data);
+            data.forEach((post) => {
+                let postContainer = document.createElement("div");
+                postContainer.classList.add("main-post");
+
+                let postHeader = document.createElement("div");
+                postHeader.classList.add("header-post");
+
+                let profilePost = document.createElement("div");
+                profilePost.classList.add("profil-post");
+
+                let userDOM = document.createElement("div");
+                userDOM.classList.add("name-date-post");
+                userDOM.innerText = post.username;
+
+                let contentPost = document.createElement("div");
+                contentPost.classList.add("content-post");
+                contentPost.innerText = post.post_text;
+                let date = document.createElement("span");
+                date.classList.add("date");
+                date.innerText = post.post_date;
+
+                if (post.post_img !== null) {
+                    let PostImg = document.createElement("img");
+                    PostImg.classList.add("postImg");
+                    PostImg.src = post.post_img;
+                    postHeader.appendChild(PostImg);
+                }
+
+                postContainer.appendChild(postHeader);
+                postHeader.appendChild(profilePost);
+                profilePost.appendChild(userDOM);
+                contentPost.appendChild(date);
+                postContainer.appendChild(contentPost);
+                allPost.appendChild(postContainer);
+            });
+        });
+};
+setInterval(fetchPost(), 1000)
+</script>
 </body>
 
 </html>
