7ee0952c8c29e3c7f2df328a9fb2ad38ff4645d9
Setsu <60669080+Setsudan@users.noreply.github.com>
2022-05-18 15:44:26 +0100

    Correction de bug d'affichage

--- includes/getUserFriends.inc.php
+++ includes/getUserFriends.inc.php
@@ -1,4 +1,7 @@
 <?php
+require "./db_connect.inc.php";
+$db = new DB();
+$bdd = $db->connectDb();
 session_start();
 $req = $bdd->prepare("SELECT COUNT(*) FROM follower WHERE user_id = ? OR follower_id =?");
 $req->execute([$_SESSION["userid"], $_SESSION["userid"]]);
